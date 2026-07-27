fc08aa0671f54e556112971179d005376659bece
Hossein <qashqaeii.ps4@gmail.com>
2026-07-27 12:55:01 +0330

    Remove unused import in ExtractScreen to streamline code

--- android/app/src/main/java/ir/divarfiling/mobile/feature/extract/ExtractScreen.kt
+++ android/app/src/main/java/ir/divarfiling/mobile/feature/extract/ExtractScreen.kt
@@ -1,6 +1,5 @@
 package ir.divarfiling.mobile.feature.extract
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
