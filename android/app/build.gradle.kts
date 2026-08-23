1bd6c3d42ff35f55573bdb3156cd8e6291e33fe1
Hossein <qashqaeii.ps4@gmail.com>
2026-08-23 12:34:51 +0330

    Update versioning to 2.6.1
    - Bump versionCode to 41 and versionName to "2.6.1" in build.gradle.kts for the upcoming release.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "ir.divarfiling.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 40
-        versionName = "2.6.0"
+        versionCode = 41
+        versionName = "2.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
