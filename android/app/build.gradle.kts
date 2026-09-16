c66e434ad9b6b782c47ad41820a8d3078b14eea7
Hossein <qashqaeii.ps4@gmail.com>
2026-09-16 12:29:47 +0330

    Update Android SDK package installation method and increment versioning to 2.6.7
    - Refactored the Android SDK package installation in build-apk.yml to use a more concise format.
    - Incremented versionCode to 47 and versionName to "2.6.7" in build.gradle.kts for the upcoming release.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "ir.divarfiling.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 46
-        versionName = "2.6.6"
+        versionCode = 47
+        versionName = "2.6.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
