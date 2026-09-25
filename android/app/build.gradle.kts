9ffe045dd4721a60b8674aac3c8a442bdc7311d3
Hossein <qashqaeii.ps4@gmail.com>
2026-09-25 11:54:24 +0330

    Increment versionCode to 52 and versionName to "2.7.2" in build.gradle.kts for the upcoming release.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "ir.divarfiling.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 51
-        versionName = "2.7.1"
+        versionCode = 52
+        versionName = "2.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
